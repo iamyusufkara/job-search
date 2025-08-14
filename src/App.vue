1235239eba842491dd944460fd7f929d73186798
Yusuf Kara <yuskara95@gmail.com>
2025-08-15 00:15:47 +0200

    Added a main nav

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,16 @@
 <script setup></script>
 
 <template>
-  <h1 class="mt-8 text-center text-6xl">Hello Vue</h1>
+  <main-nav />
 </template>
+
+<script>
+import MainNav from '@/components/MainNav.vue'
+
+export default {
+  name: 'App',
+  components: {
+    MainNav,
+  },
+}
+</script>
---
+++ src/components/MainNav.vue
@@ -0,0 +1,20 @@
+<template>
+  <header class="w-full text-sm">
+    <div class="fixed left-0 top-0 h-16 w-full bg-white">
+      <div class="border-brand-gray-1 mx-auto flex h-full flex-nowrap border-b border-solid px-8">
+        <a href="/" class="flex h-full items-center text-xl">{{ company }}</a>
+      </div>
+    </div>
+  </header>
+</template>
+
+<script>
+export default {
+  name: 'MainNav',
+  data() {
+    return {
+      company: 'Entwicklerkarriere',
+    }
+  },
+}
+</script>
