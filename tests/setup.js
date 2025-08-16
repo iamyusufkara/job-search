eb58c2b9d1931708f2c4853c00c4bb239cbc2b58
Yusuf Kara <yuskara95@gmail.com>
2025-08-16 22:53:55 +0200

    Added a test with vitest and tested the main-nav

---
+++ tests/setup.js
@@ -0,0 +1,10 @@
+import { cleanup } from '@testing-library/vue'
+import * as matchers from '@testing-library/jest-dom/matchers'
+import { expect, afterEach } from 'vitest'
+
+expect.extend(matchers)
+
+// Bei jedem Test wird einmal zurückgesetzt, damit der nächste Test
+afterEach(() => {
+  cleanup()
+})
--- vite.config.js
+++ vite.config.js
@@ -13,4 +13,8 @@ export default defineConfig({
       '@': fileURLToPath(new URL('./src', import.meta.url)),
     },
   },
+  test: {
+    globals: true,
+    setupFiles: ['./tests/setup.js'],
+  },
 })
