f116d12e2f9460f7b2fd8a960682960929905535
Yusuf Kara <yuskara95@gmail.com>
2025-08-17 22:01:28 +0200

    Added the subnav to the nav if the job page is active + tests for both navs

--- src/components/MainNav.vue
+++ src/components/MainNav.vue
@@ -2,19 +2,49 @@
   <header class="w-full text-sm">
     <div class="fixed left-0 top-0 h-16 w-full bg-white">
       <div class="border-brand-gray-1 mx-auto flex h-full flex-nowrap border-b border-solid px-8">
-        <a href="/" class="flex h-full items-center text-xl">{{ company }}</a>
+        <a :href="url" class="flex h-full items-center text-xl">{{ company }}</a>
+
+        <nav class="ml-12 h-full">
+          <ul class="flex h-full list-none">
+            <li class="ml-9 h-full first:ml-0" v-for="menuItem in menuItems" :key="menuItem">
+              <a href="" class="flex h-full items-center py-2.5">{{ menuItem }}</a>
+            </li>
+          </ul>
+        </nav>
+        <div class="ml-auto flex h-full items-center">
+          <profile-image v-if="isLoggedIn" />
+          <action-button v-else text="Login" type="primary" @click="loginUser" />
+        </div>
       </div>
+      <sub-nav v-if="isLoggedIn" />
     </div>
   </header>
 </template>
 
 <script>
+import ActionButton from './ActionButton.vue'
+import ProfileImage from './ProfileImage.vue'
+import SubNav from './SubNav.vue'
+
 export default {
   name: 'MainNav',
+  components: {
+    ActionButton,
+    ProfileImage,
+    SubNav,
+  },
   data() {
     return {
-      company: 'Entwicklerkarriere',
+      company: 'Hirely',
+      url: 'https://indeed.com',
+      menuItems: ['Unser Team', 'Standorte', 'How we hire', 'Karriere'],
+      isLoggedIn: false,
     }
+  },
+  methods: {
+    loginUser() {
+      this.isLoggedIn = true
+    },
   },
 }
 </script>
