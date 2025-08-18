fbfaff405ceb0690f75afc0156b6b7703c2d65b0
Yusuf Kara <yuskara95@gmail.com>
2025-08-18 18:58:41 +0200

    Added some general components and added some new colors

--- src/App.vue
+++ src/App.vue
@@ -2,15 +2,18 @@
 
 <template>
   <main-nav />
+  <hero-section />
 </template>
 
 <script>
-import MainNav from '@/components/MainNav.vue'
+import MainNav from '@/components/Navigation/MainNav.vue'
+import HeroSection from '@/components/JobSearch/HeroSection.vue'
 
 export default {
   name: 'App',
   components: {
     MainNav,
+    HeroSection,
   },
 }
 </script>
---
+++ src/components/Navigation/ProfileImage.vue
@@ -0,0 +1,14 @@
+<template>
+  <img :src="imageLink" class="h-8 w-8 rounded-3xl object-contain" alt="User profile image" />
+</template>
+
+<script>
+export default {
+  name: 'ProfileImage',
+  data() {
+    return {
+      imageLink: 'https://avatar.iran.liara.run/public/12',
+    }
+  },
+}
+</script>
