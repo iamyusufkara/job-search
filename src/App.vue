36238a3346ef696672302e7caf299190ea926e3c
Yusuf Kara <yuskara95@gmail.com>
2025-08-19 01:14:37 +0200

    Added the router to the main app

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,13 @@
-<script setup></script>
-
 <template>
   <main-nav />
-  <hero-section />
+  <router-view />
 </template>
 
 <script>
 import MainNav from '@/components/Navigation/MainNav.vue'
-import HeroSection from '@/components/JobSearch/HeroSection.vue'
 
 export default {
   name: 'App',
-  components: {
-    MainNav,
-    HeroSection,
-  },
+  components: { MainNav },
 }
 </script>
