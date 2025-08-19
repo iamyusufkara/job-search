03a26e1c6010f3e410b2d72bd4bde495abe1758c
Yusuf Kara <yuskara95@gmail.com>
2025-08-19 02:19:01 +0200

    Added dynamic data to the job listings and single listing to show all jobs in paginatin

--- src/components/JobResults/JobListing.vue
+++ src/components/JobResults/JobListing.vue
@@ -1,17 +1,21 @@
 <template>
   <li class="mb-7">
     <router-link
-      to="/jobs/results/1"
+      :to="jobPageLink"
       class="border-brand-gray-2 hover:shadow-gray mx-auto block rounded border border-solid bg-white"
     >
       <div class="border-brand-gray-2 mx-8 border-b border-solid pb-2 pt-5">
-        <h2 class="mb-2 text-2xl">Technical Program Manager, Perception, Augment Reality</h2>
+        <h2 class="mb-2 text-2xl">{{ job.title }}</h2>
         <div class="flex flex-row align-middle">
           <div class="mr-5">
-            <span>Bitara</span>
+            <span>{{ job.organization }}</span>
           </div>
           <div>
-            <span>Nürnberg, Bayern, Deutschland</span>
+            <ul>
+              <li v-for="location in job.locations" :key="location" class="mr-5 inline-block">
+                <span>{{ location }}</span>
+              </li>
+            </ul>
           </div>
         </div>
       </div>
@@ -20,17 +24,14 @@
           <h3 class="mb-2 mt-1">Qualifikationen:</h3>
           <div>
             <ul class="list-disc pl-8">
-              <li>Bachelor's degree or equivalent practical experience.</li>
-              <li>5 years of experience in program management.</li>
-              <li>
-                Experience analyzing data through querying database (e.g., SQL) using spreadsheet
-                software and creating statistical models.
+              <li v-for="qualification in job.minimumQualifications" :key="qualification">
+                {{ qualification }}
               </li>
             </ul>
           </div>
         </div>
         <div class="mt-2 text-center">
-          <router-link to="/jobs/results/1" class="text-brand-blue-1">Mehr anzeigen</router-link>
+          <router-link :to="jobPageLink" class="text-brand-blue-1">Mehr anzeigen</router-link>
         </div>
       </div>
     </router-link>
@@ -40,5 +41,16 @@
 <script>
 export default {
   name: 'JobListing',
+  props: {
+    job: {
+      type: Object,
+      required: true,
+    },
+  },
+  computed: {
+    jobPageLink() {
+      return `/jobs/results/${this.job.id}`
+    },
+  },
 }
 </script>
--- src/components/JobResults/JobListings.vue
+++ src/components/JobResults/JobListings.vue
@@ -1,20 +1,74 @@
 <template>
   <main class="bg-brand-gray-2 flex-auto p-8">
     <ol>
-      <job-listing />
-      <job-listing />
-      <job-listing />
+      <job-listing v-for="job in displayedJobs" :key="job.id" :job="job" />
     </ol>
+
+    <div class="mx-auto mt-8">
+      <div class="flex flex-row flex-nowrap">
+        <p class="flex-grow text-sm">Seite {{ currentPage }}</p>
+
+        <div class="flex items-center justify-center">
+          <router-link
+            v-if="previousPage"
+            :to="{ name: 'JobResults', query: { page: previousPage } }"
+            class="text-brand-blue-1 mx-3 text-sm font-semibold"
+            >Zurück</router-link
+          >
+
+          <router-link
+            v-if="nextPage"
+            role="link"
+            :to="{ name: 'JobResults', query: { page: nextPage } }"
+            class="text-brand-blue-1 mx-3 text-sm font-semibold"
+            >Weiter</router-link
+          >
+        </div>
+      </div>
+    </div>
   </main>
 </template>
 
 <script>
+import axios from 'axios'
 import JobListing from '@/components/JobResults/JobListing.vue'
 
 export default {
   name: 'JobListings',
   components: {
     JobListing,
+  },
+  data() {
+    return {
+      jobs: [],
+    }
+  },
+  computed: {
+    currentPage() {
+      return Number.parseInt(this.$route.query.page || '1')
+    },
+    previousPage() {
+      const previousPage = this.currentPage - 1
+      const firstPage = 1
+      return previousPage >= firstPage ? previousPage : undefined
+    },
+    nextPage() {
+      const nextPage = this.currentPage + 1
+      const lastPage = Math.ceil(this.jobs.length / 10)
+      return nextPage <= lastPage ? nextPage : undefined
+    },
+    displayedJobs() {
+      const pageString = this.currentPage
+      const pageNumber = Number.parseInt(pageString)
+      const firstJobIndex = (pageNumber - 1) * 10
+      const lastJobIndex = pageNumber * 10
+      return this.jobs.slice(firstJobIndex, lastJobIndex)
+    },
+  },
+  async mounted() {
+    const baseUrl = import.meta.env.VITE_APP_API_URL
+    const response = await axios.get(`${baseUrl}/jobs`)
+    this.jobs = response.data
   },
 }
 </script>
