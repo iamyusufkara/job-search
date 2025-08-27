ea623427ce2e62ef72b39c97924feae2a4d44005
Yusuf Kara <yuskara95@gmail.com>
2025-08-28 00:56:04 +0200

    Updated the subnav

--- src/components/Navigation/SubNav.vue
+++ src/components/Navigation/SubNav.vue
@@ -3,16 +3,22 @@
     <div class="flex h-full items-center px-8">
       <div v-if="onJobResultsPage">
         <font-awesome-icon :icon="['fas', 'search']" class="mr-3" />
-        <span><span class="text-brand-green-1">1653</span> Jobs gefunden</span>
+        <span
+          ><span class="text-brand-green-1">{{ FILTERED_JOBS.length }}</span> Jobs gefunden</span
+        >
       </div>
     </div>
   </div>
 </template>
 
 <script>
+import { mapState } from 'pinia'
+import { useJobsStore, FILTERED_JOBS } from '@/stores/jobs'
+
 export default {
   name: 'SubNav',
   computed: {
+    ...mapState(useJobsStore, [FILTERED_JOBS]),
     onJobResultsPage() {
       return this.$route.name === 'JobResults'
     },
