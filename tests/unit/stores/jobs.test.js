d088e6159f1b1faf6d57a99324a4b04e57aab968
Yusuf Kara <yuskara95@gmail.com>
2025-08-28 00:53:32 +0200

    Added new getters for user and jobs + tests

--- tests/unit/stores/jobs.test.js
+++ tests/unit/stores/jobs.test.js
@@ -53,40 +53,12 @@ describe('getters', () => {
     })
   })
 
-  describe('FILTERED_JOBS_BY_ORGANIZATIONS', () => {
-    it('identifies jobs that are associated with the given organizations', () => {
-      const jobsStore = useJobsStore()
-      jobsStore.jobs = [
-        { organization: 'Google' },
-        { organization: 'Amazon' },
-        { organization: 'Microsoft' },
-      ]
-      const userStore = useUserStore()
-      userStore.selectedOrganizations = ['Google', 'Microsoft']
-
-      const result = jobsStore.FILTERED_JOBS_BY_ORGANIZATIONS
-      expect(result).toEqual([{ organization: 'Google' }, { organization: 'Microsoft' }])
-    })
-
-    describe('when the user has not selected any organizations', () => {
-      it('return all jobs', () => {
-        const jobsStore = useJobsStore()
-        jobsStore.jobs = [
-          { organization: 'Google' },
-          { organization: 'Amazon' },
-          { organization: 'Microsoft' },
-        ]
-        const userStore = useUserStore()
-        userStore.selectedOrganizations = []
-
-        const result = jobsStore.FILTERED_JOBS_BY_ORGANIZATIONS
-
-        expect(result).toEqual([
-          { organization: 'Google' },
-          { organization: 'Amazon' },
-          { organization: 'Microsoft' },
-        ])
-      })
+  describe('UNIQUE_JOB_TYPES', () => {
+    it('finds unique job types from list of jobs', () => {
+      const store = useJobsStore()
+      store.jobs = [{ jobType: 'Full-time' }, { jobType: 'Temporary' }, { jobType: 'Part-time' }]
+      const result = store.UNIQUE_JOB_TYPES
+      expect(result).toEqual(new Set(['Full-time', 'Temporary']))
     })
   })
 })
